be9b03ca0d13a2bd7806f5d4d50111e3bba6a338
= <=>
2026-01-24 15:50:15 +0800

    exercise 3 - continuation

--- test3.py
+++ test3.py
@@ -1,3 +1,11 @@
+print("""
+TODO:
+### 3. User Input
+- Ask user for name
+- Ask user for age
+- Print a greeting using input values
+      """)
+
 name = input("What is your name: ")
 
 print("Welcome to python " + name) 
