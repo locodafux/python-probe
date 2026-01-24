d659929f25f366367bc880199acfb9701c26ea47
= <=>
2026-01-24 11:07:02 +0800

    exercise 2

---
+++ test2.py
@@ -0,0 +1,30 @@
+print("""Create variables 
+  - string
+  - integer
+  - float
+  - boolean\n\n""")
+
+y = "Leo"
+x = "27" 
+w = 27 
+a = 19.1
+b = True
+
+print(w)
+print(type(w))
+
+print(y)
+print(type(y))
+
+print(a)
+print(type(a))
+
+print(b)
+print(type(b))
+print("Converting string to integer")
+intX = int(x)
+print(type(intX)) 
+print(y)
+print(intX + intX)
+print(y + " is " + str(intX) + " years old today.")
+print(y + " is " + str(intX + 5) + " in 5 years.") 
