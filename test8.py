8e236b85b4fe49ffff55f7c2bb9b49924f78a771
= <=>
2026-01-28 22:37:05 +0800

    exercise 8

---
+++ test8.py
@@ -0,0 +1,23 @@
+print("""8. Functions
+- Create a function
+- Use parameters
+- Return values
+- Call functions multiple times
+""")
+
+def myFunction():
+    print('This is a function')
+
+myFunction()
+myFunction()
+
+def printName(name):
+    print('Hello ' + name)
+
+printName('Leo')
+
+def sumOfIntegers(x,y):
+    return x+y
+
+print(sumOfIntegers(5,10))
+
