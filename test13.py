b2a82540543a97dfa24da81b5a2d70f92103247c
= <=>
2026-02-01 21:30:29 +0800

    exercise 13

---
+++ test13.py
@@ -0,0 +1,14 @@
+print("""### 13. Virtual Environments
+- Create a virtual environment
+- Activate / deactivate venv
+- Install packages inside venv
+""")
+
+#This is the command to creae a virtual environment in macos/linux
+python3 -m venv myfirstproject
+
+#To Activate the virtual environment in macos/linux
+source myfirstproject/bin/activate
+
+#To deactivate the virtual environment just type
+deactivate
