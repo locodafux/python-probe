8dbc93a3c0782f30a148384647fe2bb3cd47cfbc
= <=>
2026-02-01 21:17:42 +0800

    exercise 12

---
+++ test12.py
@@ -0,0 +1,14 @@
+import module as mod 
+from module import greet
+print("""### 12. Modules & Imports
+- Split code into multiple `.py` files
+- Import functions from another file
+- Organize project structure
+""")
+
+mod.greet("Ann")
+greet("leo")
+
+
+
+
