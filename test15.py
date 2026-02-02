fda14235d076e1589523998796f5cafb07ea28d2
= <=>
2026-02-02 23:23:59 +0800

    exercise 15

--- test15.py
+++ test15.py
@@ -21,3 +21,22 @@ data = {
 
 with open("data.json", "w") as file:
     json.dump(data, file, indent=4)
+
+with open("data.json", "r") as file:
+    data = json.load(file) 
+
+data['favorites'] = "Coding"
+
+with open("data.json", "w") as file:
+    json.dump(data, file, indent=4)
+
+with open("data.json", "r") as file:
+    data = json.load(file) 
+    
+del data['favorites']
+print(data)
+
+with open("data.json", "w") as file:
+    data = json.dump(data, file, indent=4)
+
+
